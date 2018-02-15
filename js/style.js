9bbc38ad969e6c1ccf1e28f6d6ce6fd25cb042ba
mre <mre@ableton.com>
2018-02-15 13:29:00 +0100

    hide footer on mobile device

--- js/style.js
+++ js/style.js
@@ -7,28 +7,34 @@ $( document ).ready(function() {
 
     var windowWidth = $(window).width();
 
-    // Don't do anything under 600
-    if (windowWidth < 600) return;
 
-
-    var targetHeight = (windowWidth > 900) ? "40vh": "35vh";
-    var typeSize = (windowWidth > 900) ? "130%": "100%";
-
-    $('#header').animate ({
-      height: targetHeight
-    },{
-      duration:300
+    if (windowWidth < 600)
+    {
+      // hide footer on mobile device
+      $(".footer").fadeOut();
     }
-    );
+    else
+    {
+      var targetHeight = (windowWidth > 900) ? "40vh": "35vh";
+      var typeSize = (windowWidth > 900) ? "130%": "100%";
+
+      $('#header').animate ({
+        height: targetHeight
+      },{
+        duration:300
+      }
+      );
 
 
-    $('#heading-primary--main').animate({
-      fontSize: typeSize,
-      marginTop:'5rem',
-      width:'50%'
-    },{
-      duration : 300
+      $('#heading-primary--main').animate({
+        fontSize: typeSize,
+        marginTop:'5rem',
+        width:'50%'
+      },{
+        duration : 300
+      }
     }
+
   );
 
     //}
